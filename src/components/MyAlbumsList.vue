43353bf8b44bdeef6723bbc6a206abddf47ffb3f
matte887 <m.erroi92@gmail.com>
2022-05-20 17:15:55 +0200

    Bonus: work in progress...

--- src/components/MyAlbumsList.vue
+++ src/components/MyAlbumsList.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container">
-    <MySelection @selectedGenre="pickSelectedGenre"/>
+    <MySelection @selectedGenre="pickSelectedGenre($event)" @selectedArtist="pickSelectedArtist($event)"/>
     <MyLoadingPage v-if="loading" />
     <div v-else class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-5 my-5">
-      <MyAlbum v-for="item in albums" :key="item.index" :albumData="item" />
+      <MyAlbum v-for="item in filterByGenre" :key="item.index" :albumData="item" />
     </div>
   </div>
 </template>
@@ -16,24 +16,46 @@ import axios from "axios";
 
 export default {
   name: "MyAlbumsList",
+
   components: {
     MyAlbum,
     MyLoadingPage,
     MySelection,
   },
+
   data() {
     return {
       albums: [],
       loading: true,
-      selectedGenre: ""
+      selected: "",
+      // selectedArtist: ""
     };
   },
+
   methods: {
     pickSelectedGenre(genreSelection) {
-      this.selectedGenre = genreSelection;
-      console.log(this.selectedGenre);
+      this.selected = genreSelection;
+    },
+    pickSelectedArtist(genreSelection) {
+      this.selected = genreSelection;
     }
   },
+
+  computed: {
+    filterByGenre() {
+      const filteredByGenre = this.albums.filter((item) => {
+        return item.genre.toLowerCase().includes(this.selected.toLowerCase());
+      });
+      return filteredByGenre;
+    },
+    filterByArtist() {
+      const filteredArtist = this.albums.filter((item) => {
+        return item.author.toLowerCase().includes(this.selected.toLowerCase());
+      });
+      return filteredArtist;
+    },
+  },
+
   created() {
     axios
       .get("https://flynn.boolean.careers/exercises/api/array/music")
--- src/components/MySelection.vue
+++ src/components/MySelection.vue
@@ -1,30 +1,47 @@
 <template>
-  <div>
-    <select name="selectGenre" id="" v-model="genreSelection" @change="$emit('selectedGenre', genreSelection)">
+  <form class="pt-4">
+    <label for="selectGenre" class="me-3">Filtra per genere musicale</label>
+    <select
+      name="selectGenre"
+      id="selectGenre"
+      v-model="genreSelection"
+      @change="$emit('selectedGenre', genreSelection)"
+    >
       <!-- <option disabled value="">Choose a genre</option> -->
       <option value=""></option>
       <option value="rock">Rock</option>
       <option value="pop">Pop</option>
       <option value="jazz">Jazz</option>
+      <option value="metal">Metal</option>
     </select>
-  </div>
+
+    <label for="selectArtist" class="me-3">Filtra per artista</label>
+    <select
+      name="selectArtist"
+      id="selectArtist"
+      v-model="artistSelection"
+      @change="$emit('selectedArtist', artistSelection)"
+    >
+      <option value=""></option>
+      <option value="Bon Jovi">Bon Jovi</option>
+    </select>
+  </form>
 </template>
 
 <script>
 export default {
   name: "MySelection",
   data() {
-      return {
-          genreSelection: ""
-      }
+    return {
+      genreSelection: "",
+      artistSelection: "",
+    };
   },
-//   methods: {
-//     pickGenre(selezione) {
-//       console.log(selezione);
-//     },
-//   },
 };
 </script>
 
 <style lang="scss" scoped>
+label {
+  color: white;
+}
 </style>
