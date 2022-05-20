cd7b6e955b4e95b2a362f686257c45e13f51f44a
matte887 <m.erroi92@gmail.com>
2022-05-20 15:26:26 +0200

    Passato dato da figlio a genitore

--- src/components/MyAlbumsList.vue
+++ src/components/MyAlbumsList.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <MySelection @selectedGenre="pickSelectedGenre"/>
     <MyLoadingPage v-if="loading" />
     <div v-else class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-5 my-5">
       <MyAlbum v-for="item in albums" :key="item.index" :albumData="item" />
@@ -9,6 +10,7 @@
 
 <script>
 import MyLoadingPage from "./MyLoadingPage.vue";
+import MySelection from "./MySelection.vue";
 import MyAlbum from "./MyAlbum.vue";
 import axios from "axios";
 
@@ -17,12 +19,20 @@ export default {
   components: {
     MyAlbum,
     MyLoadingPage,
+    MySelection,
   },
   data() {
     return {
       albums: [],
       loading: true,
+      selectedGenre: ""
     };
+  },
+  methods: {
+    pickSelectedGenre(genreSelection) {
+      this.selectedGenre = genreSelection;
+      console.log(this.selectedGenre);
+    }
   },
   created() {
     axios
---
+++ src/components/MySelection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <select name="selectGenre" id="" v-model="genreSelection" @change="$emit('selectedGenre', genreSelection)">
+      <!-- <option disabled value="">Choose a genre</option> -->
+      <option value=""></option>
+      <option value="rock">Rock</option>
+      <option value="pop">Pop</option>
+      <option value="jazz">Jazz</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MySelection",
+  data() {
+      return {
+          genreSelection: ""
+      }
+  },
+//   methods: {
+//     pickGenre(selezione) {
+//       console.log(selezione);
+//     },
+//   },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
